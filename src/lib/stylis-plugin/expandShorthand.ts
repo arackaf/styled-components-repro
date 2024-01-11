7577b181d3a1caa6dd4fab4f390f30dd0d2dcef4
Adam Rackis <adamrackis@hotmail.com>
2024-01-11 13:09:13 -0600

    test left property with css`` helper

--- src/lib/stylis-plugin/expandShorthand.ts
+++ src/lib/stylis-plugin/expandShorthand.ts
@@ -1,24 +1,19 @@
-export function expandShorthand(
-  property: string,
-  value: string,
-): [string, string][] {
-  const shorthandProperties = ['margin', 'padding'];
+export function expandShorthand(property: string, value: string): [string, string][] {
+  const shorthandProperties = ["margin", "padding"];
 
   if (!shorthandProperties.includes(property)) {
     return [[property, value]];
   }
   // eslint-disable-next-line no-param-reassign
-  value = value.replace(/var\(.+?\)/g, match => match.replace(/\s+/g, ''));
+  value = value.replace(/var\(.+?\)/g, (match) => match.replace(/\s+/g, ""));
 
-  const [top, right = top, bottom = top, left = right] = value
-    .trim()
-    .split(/\s+/);
+  const [top, right = top, bottom = top, left = right] = value.trim().split(/\s+/);
 
   if (left !== right) {
-    if (process.env.NODE_ENV === 'development') {
+    if (process.env.NODE_ENV === "development") {
       // eslint-disable-next-line no-console
       console.error(
-        `Avoid using the ${property} shorthand with different left/right values since this won't translate correctly to RTL languages. Instead, use ${property}-inline-start|end ---> ${property}: ${value}`,
+        `Avoid using the ${property} shorthand with different left/right values since this won't translate correctly to RTL languages. Instead, use ${property}-inline-start|end ---> ${property}: ${value}`
       );
     }
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,40 +13,11 @@ const red = css`
 `;
 
 const Junk = styled.div<Props>`
-  font-size: 20px;
-
-  margin-left: 5px;
-  padding-right: 15px;
-  padding-left: 55px;
-
-  [dir="rtl"] & {
-    color: green;
-
-    margin-top: 2px;
-    margin-left: 50px;
-    margin-right: 51px;
-  }
-
-  @media (min-width: 200px) {
-    span {
-      text-decoration: underline;
-      border-left: 2px solid purple;
-    }
-  }
-  @media (min-width: 200px) {
-    [dir="rtl"] & {
-      span {
-        line-height: 3;
-      }
-    }
-  }
-
-  @media (min-width: 200px) {
-    border: 1px solid red;
-  }
-  @media (max-width: 200px) {
-    border: 1px solid blue;
-  }
+  position: absolute;
+  ${() =>
+    css`
+      left: 100px;
+    `}
 `;
 
 export default function Home() {
@@ -67,23 +38,8 @@ export default function Home() {
   }, []);
 
   return rendered ? (
-    <main>
-      <div dir="rtl">
-        <div>
-          <Junk $active={active}>
-            <span>Hello World</span>
-          </Junk>
-        </div>
-      </div>
-      <br />
-      <br />
-      <div>
-        <div>
-          <Junk $active={active}>
-            <span>Hello World</span>
-          </Junk>
-        </div>
-      </div>
+    <main dir="rtl" style={{ position: "relative", padding: "20px" }}>
+      <Junk>Hello World</Junk>
     </main>
   ) : null;
 }
